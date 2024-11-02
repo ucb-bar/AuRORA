8289b21664404e294a5c88813f3c29c1ced93946
Cloud User <centos@ip-192-168-2-240.ec2.internal>
2024-11-02 06:22:22 +0000

    multiple rounds

--- bareMetalC/mt_contention.c
+++ bareMetalC/mt_contention.c
@@ -15,7 +15,7 @@
 #include "aurora-sw/aurora.h"
 #include "aurora-sw/gemmini_aurora.h"
 
-#define TURN 30
+#define TURN 15
 
 #define NUM_ARRAY 4
 void thread_entry (int cid, int nc){
@@ -23,47 +23,48 @@ void thread_entry (int cid, int nc){
       if (i == cid) printf("Thread %d/%d starting\n", cid, nc);
       barrier(nc);
     }
-    int num_requested_array[TURN] = {0};
-    int num_acquired_array[TURN] = {0};
-    for(int j = 0; j < nc; j++){
-      if(j == cid){
-        for(int t = 0; t < TURN; t++){ 
-          int requested_array = (cid + rand()) % NUM_ARRAY; 
-          if (requested_array == 0){
-            requested_array = NUM_ARRAY - 1;
+    for(int n = 0; n < 3; n++){
+      int num_requested_array[TURN] = {0};
+      int num_acquired_array[TURN] = {0};
+      for(int j = 0; j < nc; j++){
+        if(j == cid){
+          for(int t = 0; t < TURN; t++){ 
+            int requested_array = (cid + rand()) % NUM_ARRAY; 
+            if (requested_array == 0){
+              requested_array = NUM_ARRAY - 1;
+            }
+            // number of requested accels
+            num_requested_array[t] = requested_array;
+            // attempt to acquire
+            int acquired_array = multi_acquire(0, requested_array, 0, NUM_ARRAY);
+            // number of acquired accels
+            num_acquired_array[t] = acquired_array;
+            for (int i = 0; i < acquired_array; i++) {
+              rr_set_opc(XCUSTOM_ACC, i);
+              gemmini_flush(0);
+            }
+            // release
+            multi_release(0, acquired_array);
           }
-          // number of requested accels
-          num_requested_array[t] = requested_array;
-          // attempt to acquire
-          int acquired_array = multi_acquire(0, requested_array, 0, NUM_ARRAY);
-          // number of acquired accels
-          num_acquired_array[t] = acquired_array;
-          for (int i = 0; i < acquired_array; i++) {
-            rr_set_opc(XCUSTOM_ACC, i);
-            gemmini_flush(0);
-          }
-          // release
-          multi_release(0, acquired_array);
         }
-      }
-      
-    }
-    barrier(nc);
-
-    for(int i = 0; i < nc; i++){
-      if(i == cid){
-        printf("thread %d array req: ", i);
-        for (int t = 0; t < TURN; t++){
-          printf("%d, ", num_requested_array[t]);
-        }
-        printf("\n");
-        printf("thread %d array use: ", i);
-        for (int t = 0; t < TURN; t++){
-          printf("%d, ", num_acquired_array[t]);
-        }
-        printf("\n");
       }
       barrier(nc);
+
+      for(int i = 0; i < nc; i++){
+        if(i == cid){
+          printf("round %d thread %d array req: ", n, i);
+          for (int t = 0; t < TURN; t++){
+            printf("%d, ", num_requested_array[t]);
+          }
+          printf("\n");
+          printf("round %d thread %d array use: ", n, i);
+          for (int t = 0; t < TURN; t++){
+            printf("%d, ", num_acquired_array[t]);
+          }
+          printf("\n");
+        }
+        barrier(nc);
+      }
     }
 
     exit(0);
